e7e2fcbda66d80b1ab26d68ba4ec0a06700cf18c
Вячеслав Григорьев <admirallkek@gmail.com>
2022-02-07 21:52:37 +0500

    итоговое задание

---
+++ Netology_IB_Instruments2Tests/Netology_IB_Instruments2Tests.swift
@@ -0,0 +1,33 @@
+//
+//  Netology_IB_Instruments2Tests.swift
+//  Netology_IB_Instruments2Tests
+//
+//  Created by Григорьев Слава on 06.02.2022.
+//
+
+import XCTest
+@testable import Netology_IB_Instruments2
+
+class Netology_IB_Instruments2Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
